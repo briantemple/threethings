073c81236a1e613627145290a217c604b8b8cea8
Brian Temple <brian@briantemple.com>
2013-05-21 22:29:13 -0600

    Update database to PostgreSQL for local deployments.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130508144507) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: true do |t|
     t.string   "name"
     t.integer  "status",       default: 0
